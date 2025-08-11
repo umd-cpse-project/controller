664631a5d311515caed4a02d888603e084e50bcf
jay3332 <40323796+jay3332@users.noreply.github.com>
2025-08-11 14:53:26 -0400

    end effector rework

--- subsystems/end_effector.py
+++ subsystems/end_effector.py
@@ -15,53 +15,24 @@ class EndEffector:
     """
 
     __slots__ = (
-        "_left_servo", "_right_servo",
-        "_pitch", "_roll",
+        '_left_servo', '_right_servo', '_neutral',
     )
 
     def __init__(
-        self,
-        left_servo: Servo,
-        right_servo: Servo,
+        self, left_servo: Servo, right_servo: Servo,
     ) -> None:
         self._left_servo: Servo = left_servo
         self._right_servo: Servo = right_servo
+        self._neutral = self._left_servo.angle, self._right_servo.angle
         
-        self._pitch = 0.0
-        self._roll = 0.0
-        self._apply()
+    def set(self, *, pitch: float | None = None, roll: float | None = None) -> None:
+        l, r = self._neutral
+        self._left_servo.angle = l + (pitch or 0.0) - (roll or 0.0)
+        self._right_servo.angle = r + (pitch or 0.0) + (roll or 0.0)
 
-    def _clamp_servo(self, angle: float) -> float:
-        """Clamp a servo angle to physical limits."""
-        return max(0, min(self._left_servo.inner.actuation_range, angle))
 
-    def _apply(self) -> None:
-        """Send the current pitch/roll values to the servos."""
-        self._left_servo.angle = self._clamp_servo(self.pitch + self.roll)
-        self._right_servo.angle = self._clamp_servo(self.pitch - self.roll)
-
-    @property
-    def pitch(self) -> float:
-        """Pitch in degrees."""
-        return self._pitch
-
-    @pitch.setter
-    def pitch(self, value: float) -> None:
-        self._pitch = value
-        self._apply()
-
-    @property
-    def roll(self) -> float:
-        """Roll in degrees."""
-        return self._roll
-
-    @roll.setter
-    def roll(self, value: float) -> None:
-        self._roll = value
-        self._apply()
-
-    def set(self, pitch: float, roll: float) -> None:
-        """Set both pitch and roll at once."""
-        self._pitch = pitch
-        self._roll = roll
-        self._apply()
+if __name__ == '__main__':
+    left = Servo(0, angle=135.0)
+    right = Servo(1, reverse=True, angle=135.0)
+    end_effector = EndEffector(left, right)
+    end_effector.set(pitch=10.0)
