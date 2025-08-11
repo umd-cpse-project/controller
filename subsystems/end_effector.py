4e7d64a0962c51651dad2408871ee71020372b97
jay3332 <40323796+jay3332@users.noreply.github.com>
2025-08-11 14:57:22 -0400

    add mutator methods to EndEffector

--- subsystems/end_effector.py
+++ subsystems/end_effector.py
@@ -16,6 +16,7 @@ class EndEffector:
 
     __slots__ = (
         '_left_servo', '_right_servo', '_neutral',
+        '_last_pitch', '_last_roll',
     )
 
     def __init__(
@@ -23,12 +24,46 @@ class EndEffector:
     ) -> None:
         self._left_servo: Servo = left_servo
         self._right_servo: Servo = right_servo
+
         self._neutral = self._left_servo.angle, self._right_servo.angle
+        self._last_pitch: float = 0.0
+        self._last_roll: float = 0.0
         
     def set(self, *, pitch: float | None = None, roll: float | None = None) -> None:
         l, r = self._neutral
-        self._left_servo.angle = l + (pitch or 0.0) - (roll or 0.0)
-        self._right_servo.angle = r + (pitch or 0.0) + (roll or 0.0)
+        pitch = pitch or self._last_pitch
+        roll = roll or self._last_roll
+        
+        self._left_servo.angle = l + pitch - roll
+        self._right_servo.angle = r + pitch - roll
+        self._last_pitch = pitch
+        self._last_roll = roll
+        
+    def reset(self) -> None:
+        """Reset the end effector to its neutral position."""
+        self._left_servo.angle, self._right_servo.angle = self._neutral
+        self._last_pitch = 0.0
+        self._last_roll = 0.0
+
+    @property
+    def pitch(self) -> float:
+        """The current pitch of the end effector."""
+        return self._last_pitch
+    
+    @pitch.setter
+    def pitch(self, value: float) -> None:
+        """Set the pitch of the end effector."""
+        self.set(pitch=value)
+    
+    @property
+    def roll(self) -> float:
+        """The current roll of the end effector."""
+        return self._last_roll
+
+    @roll.setter
+    def roll(self, value: float) -> None:
+        """Set the roll of the end effector."""
+        self.set(roll=value)
 
 
 if __name__ == '__main__':
